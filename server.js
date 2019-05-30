d8f625b6b56138a19c59c191e7990951619ad26c
Richa <merichajsr23@gmail.com>
2019-05-30 21:06:06 +0530

    deleted peojects.hbs

--- server.js
+++ server.js
@@ -41,11 +41,7 @@ app.get('/about',(req,res)=>{
         pageTitle:'About Page',
     });
 });
-app.get('/projects',(req,res)=>{
-    res.render('project.hbs',{
-        pageTitle:'Projects'
-    });
-});
+
 
 app.get('/bad',(req,res)=>{
 res.send({
